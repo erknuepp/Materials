9207eb4f4d8cde3bcd93e8f61a9ed146af3eaba0
Edward Knueppel <edward.knueppel@zywave.com>
2022-02-05 21:55:28 -0600

    Adds dictionary features

--- Assets/Scripts/SpinCollections.cs
+++ Assets/Scripts/SpinCollections.cs
@@ -10,7 +10,7 @@ public class SpinCollections : MonoBehaviour
     /// Controls the rate at which cube spins; higher is faster.
     /// Default int32: 33
     /// </summary>
-    [SerializeField, Range(0,999)]
+    [SerializeField, Range(0, 999)]
     private int SpinFactor = 33;
 
     /// <summary>
@@ -28,24 +28,41 @@ public class SpinCollections : MonoBehaviour
     private int RedColorFactor = 60;
 
     /// <summary>
-    /// Stores the name of the gameObject as the Key and a Stack of colors it has changed to as it's value
+    /// Stores the name of the gameObject as the Key and a Stack of colors it has changed to as it's Value
     /// </summary>
     private Dictionary<string, Stack<Color>> _cubeColorStack;
+
     private Stack<Color> _colorStack;
-    
+
+    private ArrayList _systemColors;
 
     private void Start()
     {
         _cubeColorStack = new Dictionary<string, Stack<Color>>();
         _colorStack = new Stack<Color>();
+        _systemColors = new ArrayList
+        {
+            Color.white,
+            Color.black,
+            Color.gray,
+            Color.clear,
+            Color.green,
+            Color.red,
+            Color.cyan,
+            Color.blue,
+            Color.yellow,
+            Color.magenta,
+            Color.grey
+
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(SpinFactor * Time.deltaTime * RotateAmount);
-        
-        Color color = Random.ColorHSV(0, 1);
+
+        Color color = Random.ColorHSV(0, 1, 0, 1, 0, 1);
 
         int red = (int)(color.r * 1000);
 
@@ -72,11 +89,20 @@ public class SpinCollections : MonoBehaviour
     {
         StringBuilder sb = new StringBuilder();
         sb.AppendLine(string.Join(',', _cubeColorStack.Keys));
-        foreach(Stack<Color> stack in _cubeColorStack.Values)
+        foreach (Stack<Color> stack in _cubeColorStack.Values)
         {
             Color color;
-            while (stack.TryPop(out color)) {
+            while (stack.TryPop(out color))
+            {
                 sb.AppendLine($"Red {color.r} Blue {color.b} Green {color.g}");
+                foreach (Color c in _systemColors)
+                {
+                    if (c.r == color.r && c.g == color.g && c.b == color.b)
+                    {
+                        sb.AppendLine($"Wow you must be lucky! {color}");
+                        Application.Quit();
+                    }
+                }
             }
         }
         Debug.Log(sb.ToString());
